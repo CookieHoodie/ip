3351fc6b91789cfc9f71cc7a2a55ec3f25e5bc2c
ch <cookiehoodie9@gmail.com>
2020-08-24 22:07:44 -0700

    Confiugure Duke to handle todo, deadline, and event commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -27,6 +27,27 @@ public class Duke {
                 doneTask.setDone(true);
                 System.out.println("Nice! I've marked this task as done: ");
                 System.out.println(doneTask);
+            } else if (command.startsWith("todo") || command.startsWith("deadline")
+            || command.startsWith("event")) {
+                System.out.println("Got it. I've added this task:");
+
+                String[] tokens = command.split(" ", 2);
+                Task newTask = null;  // to be stored into the taskList later
+
+                if (tokens[0].equals("todo")) {
+                    newTask = new Todo(tokens[1].strip());
+                } else if (tokens[0].equals("deadline")) {
+                    String[] taskAndDate = tokens[1].split("/by");
+                    newTask = new Deadline(taskAndDate[0].strip(), taskAndDate[1].strip());
+                } else if (tokens[0].equals("event")) {
+                    String[] taskAndDate = tokens[1].split("/at");
+                    newTask = new Event(taskAndDate[0].strip(), taskAndDate[1].strip());
+                }
+
+                System.out.println(newTask);
+                taskList[taskNum] = newTask;
+                taskNum++;
+                System.out.println("Now you have " + taskNum + " tasks in the list.");
             } else {
                 System.out.println("added: " + command);
                 Task newTask = new Task(command);
